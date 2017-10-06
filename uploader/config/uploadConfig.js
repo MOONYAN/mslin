9701612848f18a9b5957229d1a91bc0eafcdf622
Boruto <t106598010@ntut.org.tw>
2017-10-06 19:55:12 +0800

    refactory by uploadConfig

---
+++ uploader/config/uploadConfig.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+module.exports = {
+    id: 't106598010',
+    digest: 'd4d62eff49c43fb77fe77e37ff8eaeaa',
+    cid: 't106598010_windowsprogramming',
+    itype: 'test',
+    img: 7,
+    type: 'teacher',
+    hw: 3
+};
--- uploader/routes/dataSheets.js
+++ uploader/routes/dataSheets.js
@@ -1,6 +1,7 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
+var uploadConfig = require('../config/uploadConfig');
 
 var { filePath, sheetName } = require('../config/fileConfig');
 var XLSX = require('xlsx');
@@ -8,7 +9,10 @@ var workbook = XLSX.readFile(filePath);
 var students = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
 router.get('/', function (req, res) {
-    res.render('submit', { students: students });
+    res.render('submit', {
+        students: students,
+        uploadConfig: uploadConfig       
+    });
 });
 
 module.exports = router;
