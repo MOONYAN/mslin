f87f63c0069f5bef91ecb5315e61ab6370aec384
Boruto <t106598010@ntut.org.tw>
2017-10-06 13:16:58 +0800

    refactory by configfile

--- uploader/routes/dataSheets.js
+++ uploader/routes/dataSheets.js
@@ -2,10 +2,10 @@
 var express = require('express');
 var router = express.Router();
 
+var { filePath, sheetName } = require('../config/fileConfig');
 var XLSX = require('xlsx');
-var workbook = XLSX.readFile('Windows Homework #1.xlsx');
-var sheetNames = workbook.SheetNames;
-var students = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[0]]);
+var workbook = XLSX.readFile(filePath);
+var students = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
 router.get('/', function (req, res) {
     return res.json({ students: students });
