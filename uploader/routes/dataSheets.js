87b160165bf0ed98484737215124bfe15331fa49
Boruto <t106598010@ntut.org.tw>
2017-10-06 13:00:36 +0800

    read xlsx file

---
+++ uploader/routes/dataSheets.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+var XLSX = require('xlsx');
+var workbook = XLSX.readFile('Windows Homework #1.xlsx');
+var sheetNames = workbook.SheetNames;
+var students = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[0]]);
+
+router.get('/', function (req, res) {
+    return res.json({ students: students });
+});
+
+module.exports = router;
